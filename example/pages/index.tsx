c7ac2395fd405fe82296eeeed3e8d645c82e4c05
Jungho Bang <jungho.bang@coinbase.com>
2023-05-04 10:22:04 -0700

    chakra

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,34 +1,67 @@
 import { DeepLinkBridge } from "@/../dist";
 import { useState } from "react";
+import { Box, Button, Container, Heading, Input, Text } from "@chakra-ui/react";
 
 export default function Home() {
-  // response
-  const [response, setResponse] = useState<string | null>(null);
+  const [randomValue, setRandomValue] = useState(
+    Math.floor(Math.random() * 100) + 1
+  );
+  const [response, setResponse] = useState("");
+  const [log, setLog] = useState("");
 
   const deepLinkBridge = new DeepLinkBridge({
     callbackPath: "/callback",
     storageKeyPrefix: "my-app",
   });
 
-  // onClick
-  const onClick = async () => {
+  const handleGenerateRandomValue = () => {
+    const newRandomValue = Math.floor(Math.random() * 100) + 1;
+    setRandomValue(newRandomValue);
+    setLog(`Generated new random value: ${newRandomValue}`);
+  };
+
+  const handleMakeRequestToMyWallet = async () => {
     console.log("onClick");
     const url = "my-wallet://request?param=1";
 
-    const response = await deepLinkBridge.sendRequest({
-      deepLinkUrl: url,
-    });
-
-    setResponse(String(response));
+    try {
+      const response = await deepLinkBridge.sendRequest({
+        deepLinkUrl: url,
+      });
+      setResponse(String(response));
+      setLog("Request to My Wallet successful");
+    } catch (error) {
+      console.error(error);
+      setLog("Error making request to My Wallet");
+    }
   };
 
   return (
-    <main>
-      <h1>Deep Link Bridge Example</h1>
-      <button onClick={onClick}>Button</button>
-      <p>
-        <strong>Response:</strong> <span id="response">{response}</span>
-      </p>
-    </main>
+    <Container maxW="xl" centerContent>
+      <Heading as="h1" size="xl" mb="4">
+        My Page
+      </Heading>
+      <Box display="flex" alignItems="center">
+        <Input mb="2" type="text" value={randomValue} isReadOnly />
+      </Box>
+      <Button mb="8" colorScheme="blue" onClick={handleGenerateRandomValue}>
+        Random Value
+      </Button>
+      <Button colorScheme="green" mb="2" onClick={handleMakeRequestToMyWallet}>
+        Make Request to My Wallet
+      </Button>
+      <Box>
+        <Text fontSize="xl" fontWeight="bold" mb="2">
+          Response from My Wallet
+        </Text>
+        <Text whiteSpace="pre-wrap">{JSON.stringify(response, null, 2)}</Text>
+      </Box>
+      <Box mt="4">
+        <Text fontSize="xl" fontWeight="bold" mb="2">
+          Logging
+        </Text>
+        <Text whiteSpace="pre-wrap">{log}</Text>
+      </Box>
+    </Container>
   );
 }
